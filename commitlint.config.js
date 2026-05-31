7a1564f96b2de20260dd303d76ef28d1951ac94a
Paweł Zambrzycki <pawelzambrzycki03@gmail.com>
2026-05-31 22:14:06 +0200

    ci: correct branch-name regex and commitlint scope rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,20 @@
+const JIRA_SCOPE_RE = /^[A-Z][A-Z0-9]+-\d+$/;
+
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "scope-jira": ({ scope }) => [
+          !scope || JIRA_SCOPE_RE.test(scope),
+          "scope, when present, must be a JIRA key like ABC-123",
+        ],
+      },
+    },
+  ],
   rules: {
-    "scope-empty": [2, "never"],
+    "scope-empty": [0],
     "scope-case": [0],
-    "scope-pattern": [2, "always", /^[A-Z][A-Z0-9]+-\d+$/],
+    "scope-jira": [2, "always"],
   },
 };
